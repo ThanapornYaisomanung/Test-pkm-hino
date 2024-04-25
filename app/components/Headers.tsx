72dc9a3a05744ceff4ba7cea8009bdb9346a3e7c
Thanaporn <tanapornyaisomanung@gmail.com>
2024-04-25 13:17:12 +0700

    Update Headers.tsx

--- app/components/Headers.tsx
+++ app/components/Headers.tsx
@@ -74,7 +74,7 @@ export default function Headers() {
   return (
     <nav className=" bg-black-p dark:bg-gray-800 ">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+        <a href="/dashboard" className="flex items-center space-x-3 rtl:space-x-reverse">
           {/* <img
             src={logo}
             className="h-8"
